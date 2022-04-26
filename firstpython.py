a2d947ccaaaaf667b4fcd4fb0c05b566c9953fe2
moumiforgit <104449934+moumiforgit@users.noreply.github.com>
2022-04-26 12:04:08 -0400

    Create firstpython.py

---
+++ firstpython.py
@@ -0,0 +1,11 @@
+#comparing numbers
+x=float(3.8**5)
+y=float(9.2**2)
+if x == y :
+    print ("x and y are equal")
+elif x<y:
+    print("x is smaller",x)
+else: 
+    print ("y is smaller",y,"and x is",x)
+print("")
+print ("THANK YOU")
